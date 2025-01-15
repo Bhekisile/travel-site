f17d1827cffd60405a6e7a31fe4e0fbe2c3dfcc3
Bhekisile <bhekisilejozi@gmail.com>
2025-01-15 12:49:48 +0200

    Completed large hero block and intro content block.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
-const path = require('path');
+const { watch } = require("fs");
+const path = require("path");
 
 const postCSSPlugins = [
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('autoprefixer')
-]
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
 
 module.exports = {
-  entry: './app/assets/scripts/App.js',
+  entry: "./app/assets/scripts/App.js",
   output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
   },
-  mode: 'development',
-  watch: true,
+  devServer: {
+    // handle css and html updated without refreshing the browser
+    watchFiles: ["app/**/*.html"],
+    static: {
+      directory: path.join(__dirname, "app"),
+      watch: false,
+    },
+    hot: true,
+    port: 3000,
+  },
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }],
-      }
-    ]
-  }
-}
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { url: false } },
+          {
+            loader: "postcss-loader",
+            options: { postcssOptions: { plugins: postCSSPlugins } },
+          },
+        ],
+      },
+    ],
+  },
+};
